c4c07a89906979618f9bdd7787aa4e0fcaa8ce2b
Walaa Ali <105892469+Walaaali782@users.noreply.github.com>
2024-09-15 23:54:29 +0300

    Add files via upload

---
+++ src/main/java/com/example/hr/entity/Department.java
@@ -0,0 +1,41 @@
+package com.example.hr.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+@Entity
+@Table(name = "hr_department")
+public class Department {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private long id ;
+	
+	private String name ;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
